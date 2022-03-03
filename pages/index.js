af7fd603c85c866080a73c7de2a946ae79cc8428
Lombeira <glomenha@gmail.com>
2022-03-03 12:18:44 -0300

    Updating page

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Lombeira</a>
+          Welcome to <a href="https://nextjs.org">Lombeira ama a feda</a>
         </h1>
 
         <p className={styles.description}>
